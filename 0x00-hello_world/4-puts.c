a0f9a774eebe41097e89469fdb6c77b1a149053e
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-19 14:21:26 -0700

    Usinf puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point
- *
- * return: Always 0
+ * Description: prints "Programming is like building a multilingual puzzle", followed by a new line
+ * Return: Always 0
  */
 int main(void)
 {
