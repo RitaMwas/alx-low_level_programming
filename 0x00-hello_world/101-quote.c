5d176b9cc93b1ae78ca1f7e3eeecd5d90c49754a
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-19 15:44:54 -0700

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints text followed by a new line to standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
