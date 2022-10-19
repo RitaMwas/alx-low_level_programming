4765130026629701a6ecdf088831733cc507c1f3
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-19 14:23:35 -0700

    Using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * Description: prints "Programming is like building a multilingual puzzle", followed by a new line
+ * Description: prints text, followed by a new line
  * Return: Always 0
  */
 int main(void)
