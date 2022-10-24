17574bd34d3de7e140a3dcac6b44d2d4001d7b2a
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-24 15:08:39 -0700

    Print minutes of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: Prints every minute starting from 00:00 to 23:59
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_purchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
