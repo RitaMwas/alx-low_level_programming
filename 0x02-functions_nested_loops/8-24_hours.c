a37f37f1aa38a21d45b8cf4776cf3715c6506e5f
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-24 15:10:22 -0700

    Print minutes of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		for (m = 0; m <= 59; m++)
 		{
 			_putchar(h / 10 + '0');
-			_purchar(h % 10 + '0');
+			_putchar(h % 10 + '0');
 			_putchar(':');
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
