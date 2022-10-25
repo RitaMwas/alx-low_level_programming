19ab5d1d460a8873ccd91841612d3412856c7f86
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-25 11:56:03 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Fibonacci sequence generated
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int n, m, next, sum;
+
+	n = 1;
+	m = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++1)
+	{
+		if (n < 4000000 && (n % 2) == 0)
+		{
+			sum = sum + n;
+		}
+		next n + m;
+		n = m;
+		m = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
