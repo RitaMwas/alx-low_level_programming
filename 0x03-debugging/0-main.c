2b7d94911667a3b1a5825158e921a6b2820cccfa
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-26 13:43:02 -0700

    Testting positive pr negative functions

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Test positive or negative function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
