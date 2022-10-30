e082886d48758c614d08fb3943759f93162bebea
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-29 22:40:18 -0700

    Do not print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,14 +12,14 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; ++n)
 	{
 		if (n == '2' || n == '4')
 		{
 			continue;
 		}
 		else
-			_putchar(n);
+			_putchar(n + 48);
 	}
 	_putchar('\n');
 }
