3aadc383ed8e0d2bf3d116a0f31733d09757f1d2
Rita Mwangi <ritahmwangi@gmail.com>
2022-10-30 06:22:10 -0700

    Prints a Fizz Buzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,28 @@ int main(void)
 
 	for (i = 1; 1 <= 100; i++)
 	{
-		if (i <= 100 && i != 1)
-			printf(" ");
-		if (i % 15 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fuzz");
-		}
 		else
 		{
 			printf("%d", i);
 		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
+
 	return (0);
 }
