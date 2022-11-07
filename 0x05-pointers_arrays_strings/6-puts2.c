9d68d32ddbc58078faa6ef18e66d365884fa1898
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-07 10:19:55 -0800

    Prints every character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Entry point
+ *
+ * Description: Prints every character of a string
+ * @str: Pointer to a char
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, input;
+
+	for (i = 0; i < strlen(input); i += 2)
+	{
+		_putchar(input[i]);
+	}
+}
