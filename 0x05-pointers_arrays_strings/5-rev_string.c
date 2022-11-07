27927342295531404c905c46a1dbf62f4e8219bc
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-07 07:24:21 -0800

    Reverses a sstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ *
+ * Description: Reverses a string
+ * @s: Pointer to a char
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
