e7bf6bd5ef99f05549641ef7042e2369d8c733b1
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-07 12:37:39 -0800

    Generates a random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random password
+ *
+ * Return: The generated password
+ */
+
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+	return (0);
+}
