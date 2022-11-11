912d7cd890bfcf8a6448ce4eb2ab41aefe4670be
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-11 05:33:06 -0800

    Comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, ret;
+	int i = 0;
 
-	for (i = 0, s1[i] && s2[i]; i++)
-	{
-		if (s1[i] == s2[i])
-			continue;
-		else
-			break;
-	}
+	for (; s1[i] != 0 && s2[i] != 0; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 
-	ret = s1[i] - s2[i];
-	return (ret);
+	return (0);
 }
