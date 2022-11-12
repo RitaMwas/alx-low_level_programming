70e58e2da42cdbc05fd3bdd7732364d856fb612d
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-12 13:41:16 -0800

    Prints a buffer 10 bytes at a time

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,34 +11,41 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int o, j, i;
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
+	o = 0;
 
-		for (j = 0; j < 10; j++)
-		{
-			if ((j + i) >= size)
-				printf(" ");
-			else
-				printf("%02x", *(b + j + i));
-			if ((j % 2) != 0 && j != 0)
-				printf(" ");
-		}
-		for (j = 0; j < 10; j++)
-		{
-			if ((j + i) >= size)
-				break;
-			else if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
-				printf("%c", *(b + j + i));
-			else
-				printf(".");
-		}
-		if (i >= size)
-			continue;
-		printf("\n");
-	}
 	if (size <= 0)
+	{
 		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf(" ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '-';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
