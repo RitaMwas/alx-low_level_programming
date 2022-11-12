7e2c12424d2770926fea9d6b99c38b627029ff3e
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-12 04:11:33 -0800

    Prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * Description: Prints an integer
+ * @n: integer
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
