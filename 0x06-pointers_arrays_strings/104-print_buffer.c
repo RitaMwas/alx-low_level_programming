99018f0916c940b088113e0cec51934d12aa5cc8
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-12 13:25:36 -0800

    Prints a buffer 10 bytes at a time

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_buffer - Entry point
+ *
+ * Description: Prints a buffer 10 bytes at a time
+ * @b: Buffer to be printed
+ * @size: Number of bytes to be printed
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if ((j + i) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + j + i));
+			if ((j % 2) != 0 && j != 0)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if ((j + i) >= size)
+				break;
+			else if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
+				printf("%c", *(b + j + i));
+			else
+				printf(".");
+		}
+		if (i >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
+}
