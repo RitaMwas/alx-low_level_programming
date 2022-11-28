7f2d6d6609ffe3125093561d8f741c88250ace9c
Rita Mwangi <ritahmwangi@gmail.com>
2022-11-28 13:22:29 -0800

    Add question 2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Entry point
+ *
+ * Description: Number of elements in a list
+ * @h: Pointer to the head of list
+ * Return: Number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h != NULL)
+	{
+		a++;
+		h = h->nets
+	}
+	return (a);
+}
