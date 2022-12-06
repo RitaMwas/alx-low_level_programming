08ae5b4b201a6da7b71dd98105594b1b9068bacc
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-06 11:33:28 -0800

    Gets the bit at a specified index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_bit - Entry point
+ *
+ * Description: Gets the bit at a specified index
+ * @n: unsigned long int input
+ * @index: index of the bit
+ * Return: value of the bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+	{
+		return (0);
+	}
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
