2148548c40c75b772f2899a7c27b76b9f9bea17d
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-10 12:55:38 -0800

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,28 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t len = 0;
-	ssize_t check = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	/*Find length of content*/
-	while (text_content[len] && text_content)
-		len++;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		check = write(fd, text_content, len);
 
-	if (check == -1)
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[len])
+		len++;
+
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
 		return (-1);
-
+	}
 	close(fd);
 	return (1);
 }
