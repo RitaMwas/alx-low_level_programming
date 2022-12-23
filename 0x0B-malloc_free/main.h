37c4952cb13aea592eb60d1eab243c23bac52446
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-23 06:51:20 -0800

    Header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,9 +11,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-char **str_to_w(char *str, int height, char **arr); 
-void word_len(char *str, int *ptr);   
-int word_count(char *str);
 
 #endif
