3735864d9d47645942cb0ce8cba23cc839d34ebe
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-28 00:49:54 -0800

    Error fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,16 +18,15 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 /**
  * dog_t - Entry point
  *
  * Description: Synonym for struct dog
  */
-
 typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
