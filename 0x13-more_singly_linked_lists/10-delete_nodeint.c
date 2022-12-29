49b593b0cae3e13f350b849e7f4405d15d4f8833
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-29 04:13:33 -0800

    Deletes a node at the specified index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,30 +9,23 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current;
-	listint_t *previous;
+	listint_t *tmp = *head;
+	int ret = FAIL;
 
-	current = *head;
-	previous = *head;
-
-	if (*head == NULL)
-		return (-1);
-
-	if (index == 0)
+	if (head && *head)
 	{
-		*head = current->next;
-		free(current);
-		return (1);
+		if (index > 0)
+		{
+			ret = delete_nodeint_at_index(&tmp->next, index - 1);
+			return (ret);
+		}
+		else
+		{
+			tmp = tmp->next;
+			free(*head);
+			*head = tmp;
+			return (PASS);
+		}
 	}
-
-	while (index != 0)
-	{
-		previous = current;
-		current = current->next;
-		index--;
-	}
-	previous->next = current->next;
-	free(current);
-
-	return (1);
+	return (ret);
 }
